f2483a542cf5268fc6b4b45eeba75fe451261064
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-11 18:30:24 -0500

    Update Percolation

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,19 +4,36 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int length;
-    private int size;
+    private boolean[][] sites;
+    private WeightedQuickUnionUF unionFind;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {}
+    public Percolation(int n) {
+        validateN(n);
+        length = n;
+        sites = new boolean[length][length];
+        for (int row = 0; row < length; row += 1) {
+            for (int col = 0; col < length; col += 1) {
+                sites[row][col] = false;
+            }
+        }
+        unionFind = new WeightedQuickUnionUF(length * length);
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {}
+    public void open(int row, int col) {
+        validateSite(row, col);
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {}
+    public boolean isOpen(int row, int col) {
+        validateSite(row, col);
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) {}
+    public boolean isFull(int row, int col) {
+        validateSite(row, col);
+    }
 
     // returns the number of open sites
     public int numberOfOpenSites() {}
