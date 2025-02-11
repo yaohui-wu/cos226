0598d7e0291a1121d54c3f6be7b722c1a1fee725
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-11 17:59:04 -0500

    Update Percolation

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,21 +1,25 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {}
 
     // sample mean of percolation threshold
-    public double mean()
+    public double mean() {}
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev() {}
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo() {}
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi() {}
 
    // test client (see below)
-   public static void main(String[] args)
+   public static void main(String[] args) {}
 
 }
