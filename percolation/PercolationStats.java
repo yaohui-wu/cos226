67bd20e7927983cf41def4c150e846d774031c84
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-12 17:22:25 -0500

    Update Percolation

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -3,12 +3,17 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
-    private double[] thresholds;
+    private static final double CONFIDENCE_95 = 1.96;
+    private int numTrials; // Number of trials.
+    private double[] thresholds; // Percolation thresholds.
 
-    // perform independent trials on an n-by-n grid
+    /**
+     * Performs trials on an n-by-n grid.
+     */
     public PercolationStats(int n, int trials) {
         validate(n, trials);
-        thresholds = new double[trials];
+        numTrials = trials;
+        thresholds = new double[numTrials];
         for (int i = 0; i < trials; i += 1) {
             double threshold = experiment(n);
             thresholds[i] = threshold;
@@ -23,47 +28,60 @@ public class PercolationStats {
         }
     }
 
+    /**
+     * Performs a single trial on an n-by-n grid.
+     */
     private double experiment(int n) {
         Percolation percolation = new Percolation(n);
+        // Open sites until percolation.
         while (!percolation.percolates()) {
+            // Randomly open a site.
             int row = StdRandom.uniformInt(1, n + 1);
             int col = StdRandom.uniformInt(1, n + 1);
             percolation.open(row, col);
         }
+        // Calculate percolation threshold.
         int open = percolation.numberOfOpenSites();
         double threshold = (double) open / (n * n);
         return threshold;
     }
 
-    // sample mean of percolation threshold
+    /**
+     * Returns the sample mean of percolation thresholds.
+     */
     public double mean() {
-        if (thresholds.length == 1) {
-            return Double.NaN;
-        }
         return StdStats.mean(thresholds);
     }
 
-    // sample standard deviation of percolation threshold
+    /**
+     * Returns the sample standard deviation of percolation thresholds.
+     */
     public double stddev() {
+        if (numTrials == 1) {
+            return Double.NaN;
+        }
         return StdStats.stddev(thresholds);
     }
 
-    // low endpoint of 95% confidence interval
+    /**
+     * Returns the low endpoint of 95% confidence interval.
+     */
     public double confidenceLo() {
         return mean() - confidence();
     }
 
-    // high endpoint of 95% confidence interval
+    /**
+     * Returns the high endpoint of 95% confidence interval.
+     */
     public double confidenceHi() {
         return mean() + confidence();
     }
 
     private double confidence() {
-        double confidence = 1.96 * stddev() / Math.sqrt(thresholds.length);
+        double confidence = CONFIDENCE_95 * stddev() / Math.sqrt(numTrials);
         return confidence;
     }
 
-    // test client (see below)
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
