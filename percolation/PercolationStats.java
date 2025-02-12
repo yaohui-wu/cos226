0fc1ebf8670ff3e6b7a287ab0358581eeffb8cfe
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-12 14:09:59 -0500

    Update Percolation

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -62,11 +62,11 @@ public class PercolationStats {
 
     // test client (see below)
     public static void main(String[] args) {
-        int n = StdIn.readInt();
-        int trials = StdIn.readInt();
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
         PercolationStats stats = new PercolationStats(n, trials);
-        StdOut.println("mean = " + stats.mean());
-        StdOut.println("stddev = " + stats.stddev());
+        StdOut.println("mean                    = " + stats.mean());
+        StdOut.println("stddev                  = " + stats.stddev());
         StdOut.println("95% confidence interval = [" + stats.confidenceLo()
             + ", " + stats.confidenceHi() + "]");
     }
