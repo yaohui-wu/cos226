7689a54ddb84961ee53c17fce78e3207e091b98d
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-11 21:34:08 -0500

    Update Percolation

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int size;
     private boolean[][] sites;
-    private WeightedQuickUnionUF unionFind;
+    private WeightedQuickUnionUF connections;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -17,17 +17,36 @@ public class Percolation {
                 sites[row][col] = false;
             }
         }
-        unionFind = new WeightedQuickUnionUF(size * size);
+        connections = new WeightedQuickUnionUF(size * size + 2);
+        for (int i = 1; i <= size; i += 1) {
+            connections.union(0, i);
+            connections.union(size * size + 1, size * size + 1 - i);
+        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validateSite(row, col);
+        sites[row - 1][col - 1] = true;
+        int element = xyTo1D(row, col);
+        if (isOpen(row, col - 1)) {
+            connections.union(element, xyTo1D(row, col - 1));
+        }
+        if (isOpen(row, col + 1)) {
+            connections.union(element, xyTo1D(row, col + 1));
+        }
+        if (isOpen(row - 1, col)) {
+            connections.union(element, xyTo1D(row - 1, col));
+        }
+        if (isOpen(row + 1, col)) {
+            connections.union(element, xyTo1D(row + 1, col));
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validateSite(row, col);
+        return sites[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
@@ -63,5 +82,7 @@ public class Percolation {
         }
     }
 
-    private int xyTo1D(int x, int y) {}
+    private int xyTo1D(int x, int y) {
+        return (x - 1) * size + y - 1;
+    }
 }
