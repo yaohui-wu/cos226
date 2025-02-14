d7e2f36c449b65061c4cb85de0a0c2b7d0553fd4
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-14 12:46:58 -0500

    Update Deques and Randomized Queues

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -10,15 +10,8 @@ import edu.princeton.cs.algs4.StdOut;
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
+        // Use one RandomizedQueue object of maximum size at most K.
         RandomizedQueue<String> permutation = new RandomizedQueue<>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            permutation.enqueue(item);
-        }
-        for (int i = 0; i < k; i++) {
-            String randomItem = permutation.dequeue();
-            StdOut.println(randomItem);
-        }
     }
  }
  
