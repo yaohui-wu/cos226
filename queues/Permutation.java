72e57664bbbe840b2b4e359bff8013b7ecaf6b15
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-13 21:07:15 -0500

    Update Deques and Randomized Queues

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,6 +1,12 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Takes an integer K, reads a sequence of strings, and prints exactly K of
+ * them, uniformly at random.
+ * 
+ * @author Yaohui Wu
+ */
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
