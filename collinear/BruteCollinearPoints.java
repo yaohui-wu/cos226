5283c7d1f58a047e2ffcb15e0b7418daeb4259e1
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-15 22:17:02 -0500

    Update Collinear Points

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -1,6 +1,11 @@
 public class BruteCollinearPoints {
-    public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-    public           int numberOfSegments()        // the number of line segments
-    public LineSegment[] segments()                // the line segments
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {}
+
+    // the number of line segments
+    public int numberOfSegments() {}
+
+    // the line segments
+    public LineSegment[] segments() {}
  }
  
