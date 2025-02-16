cb1085fe9115dd5fd40ec92ed576768e08931271
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-15 22:31:43 -0500

    Update Collinear Points

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -1,6 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class BruteCollinearPoints {
     // finds all line segments containing 4 points
-    public BruteCollinearPoints(Point[] points) {}
+    public BruteCollinearPoints(Point[] points) {
+        validateArg(points);
+    }
+
+    private void validateArg(Point[] points) {
+        boolean valid = true;
+        if (points == null) {
+            valid = false;
+        }
+        List<Point> uniquePoints = new ArrayList<>();
+        for (Point point : points) {
+            if (point == null || uniquePoints.contains(point)) {
+                valid = false;
+            } else {
+                uniquePoints.add(point);
+            }
+        }
+        if (!valid) {
+            String error = "Invalid argument";
+            throw new IllegalArgumentException(error);
+        }
+    }
 
     // the number of line segments
     public int numberOfSegments() {}
