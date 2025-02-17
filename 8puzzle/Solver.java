e4aa08ba1c679bea5e8b7cd7622ee65c894de9df
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-17 09:31:06 -0500

    Update 8 Puzzle

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,18 +1,57 @@
+/**
+ * An immutable data type that implements A* search to solve n-by-n slider puzzles.
+ * 
+ * @author Yaohui Wu
+ */
 public class Solver {
 
-    // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial)
+    /**
+     * Finds a solution to the initial board using the A* algorithm.
+     */
+    public Solver(Board initial) {}
 
-    // is the initial board solvable? (see below)
-    public boolean isSolvable()
+    /**
+     * Checks if the initial board is solvable.
+     */
+    public boolean isSolvable() {}
 
-    // min number of moves to solve initial board; -1 if unsolvable
-    public int moves()
+    /**
+     * Returns the minimum number of moves to solve the initial board, returns
+     * -1 if unsolvable.
+     */
+    public int moves() {}
 
     // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution()
+    /**
+     * Returns the sequence of boards in a shortest solution, returns null if
+     * unsolvable.
+     */
+    public Iterable<Board> solution() {}
 
-    // test client (see below) 
-    public static void main(String[] args)
+    /**
+     * Test client.
+     */
+    public static void main(String[] args) {
 
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+    
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+    
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
 }
