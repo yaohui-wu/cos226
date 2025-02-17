e6101751d7ae5114b09c73b288d11240d6832e0a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-17 09:26:31 -0500

    Update 8 Puzzle

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,34 +1,52 @@
+/**
+ * An immutable data type that models an n-by-n board with sliding tiles.
+ * 
+ * @author Yaohui Wu
+ */
 public class Board {
-
-    // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles)
+    /**
+     * Create a board from an n-by-n array of tiles, where tiles[row][col]
+     * = tile at (row, col).
+     */
+    public Board(int[][] tiles) {}
                                            
-    // string representation of this board
-    public String toString()
+    /**
+     * String representation of this board.
+     */
+    public String toString() {}
 
-    // board dimension n
-    public int dimension()
+    /**
+     * Board dimension n.
+     */
+    public int dimension() {}
 
-    // number of tiles out of place
-    public int hamming()
+    /**
+     * Number of tiles out of place.
+     */
+    public int hamming() {}
 
-    // sum of Manhattan distances between tiles and goal
-    public int manhattan()
+    /**
+     * Sum of Manhattan distances between tiles and goal.
+     */
+    public int manhattan() {}
 
-    // is this board the goal board?
-    public boolean isGoal()
+    /**
+     * Checks if this board is the goal board.
+     */
+    public boolean isGoal() {}
 
-    // does this board equal y?
-    public boolean equals(Object y)
+    /**
+     * Checks if this board is equal to another board.
+     */
+    public boolean equals(Object y) {}
 
-    // all neighboring boards
-    public Iterable<Board> neighbors()
+    /**
+     * All neighboring boards.
+     */
+    public Iterable<Board> neighbors() {}
 
-    // a board that is obtained by exchanging any pair of tiles
-    public Board twin()
-
-    // unit testing (not graded)
-    public static void main(String[] args)
-
+    /**
+     * A board that is obtained by exchanging any pair of tiles.
+     */
+    public Board twin() {}
 }
