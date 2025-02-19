6711102b1e6743b5981f3e036643bf60cbe7cd0c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-19 14:15:14 -0500

    Update Kd-Trees

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -13,7 +13,7 @@ public class KdTree {
 
     // construct an empty set of points
     public KdTree() {
-        root = new Node(null, null, null);
+        root = null;
         size = 0;
     }
 
@@ -30,6 +30,25 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         validateArg(p);
+        Node node = new Node(p);
+        insert(root, node, false);
+    }
+
+    private void insert(Node root, Node node, boolean compareY) {
+        if (root == null) {
+            root = node;
+        }
+        double rootKey = root.p.x();
+        double nodeKey = node.p.x();
+        if (compareY) {
+            rootKey = root.p.y();
+            nodeKey = node.p.y();
+        }
+        if (nodeKey < rootKey) {
+            root = root.lb;
+        } else {
+            root = root.rt;
+        }
     }
 
     // does the set contain point p?
@@ -64,10 +83,11 @@ public class KdTree {
         private Node lb; // the left/bottom subtree
         private Node rt; // the right/top subtree
 
-        public Node(Point2D point, Node leftBottom, Node rightTop) {
+        public Node(Point2D point) {
             p = point;
-            lb = leftBottom;
-            rt = rightTop;
+            rect = null;
+            lb = null;
+            rt = null;
         }
     }
 
