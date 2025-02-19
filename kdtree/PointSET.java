62e14a3e937562335343321a5ed957cb4e42cfd5
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-19 10:58:32 -0500

    Update Kd-Trees

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -23,6 +23,13 @@ public class PointSET {
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {}
 
+    private void validateArg(Object obj) {
+        if (obj == null) {
+            String error = "Argument cannot be null";
+            throw new IllegalArgumentException(error);
+        }
+    }
+
     // unit testing of the methods (optional)
     public static void main(String[] args) {}
 }
