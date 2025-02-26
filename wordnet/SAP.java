ee5da81e8f16573f46f0ec2205e3b72ee5291045
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-25 22:17:22 -0500

    Update Shortest ancestral path

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -4,23 +4,37 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
-
-    // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G)
+    /**
+     * Constructor takes a digraph (not necessarily a DAG).
+     */
+    public SAP(Digraph G) {}
  
-    // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w)
+    /**
+     * Length of shortest ancestral path between v and w; -1 if no such path.
+     */
+    public int length(int v, int w) {}
  
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w)
+    /**
+     * A common ancestor of v and w that participates in a shortest ancestral
+     * path; -1 if no such path.
+     */
+    public int ancestor(int v, int w) {}
  
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w)
+    /**
+     * Length of shortest ancestral path between any vertex in v and any
+     * vertex in w; -1 if no such path.
+     */
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {}
  
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+    /**
+     * A common ancestor that participates in shortest ancestral path; -1 if
+     * no such path.
+     */
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {}
  
-    // do unit testing of this class
+    /**
+     * Do unit testing of this class.
+     */
     public static void main(String[] args) {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
