37bdfab35560fc67c59acfaa5e1ac8df7735c9db
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-25 22:10:42 -0500

    Update WordNet

--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -1,21 +1,32 @@
 public class WordNet {
-
-    // constructor takes the name of the two input files
-    public WordNet(String synsets, String hypernyms)
+    /**
+     * Constructor takes the name of the two input files.
+     */
+    public WordNet(String synsets, String hypernyms) {}
  
-    // returns all WordNet nouns
-    public Iterable<String> nouns()
+    /**
+     * Returns all WordNet nouns.
+     */
+    public Iterable<String> nouns() {}
  
-    // is the word a WordNet noun?
-    public boolean isNoun(String word)
+    /**
+     * Returns true if the word is a WordNet noun.
+     */
+    public boolean isNoun(String word) {}
  
-    // distance between nounA and nounB (defined below)
-    public int distance(String nounA, String nounB)
+    /**
+     * Distance between nounA and nounB (defined below).
+     */
+    public int distance(String nounA, String nounB) {}
  
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
-    public String sap(String nounA, String nounB)
+    /**
+     * A synset (second field of synsets.txt) that is the common ancestor of
+     * nounA and nounB.
+     */
+    public String sap(String nounA, String nounB) {}
  
-    // do unit testing of this class
-    public static void main(String[] args)
+    /**
+     * Do unit testing of this class.
+     */
+    public static void main(String[] args) {}
 }
