84a79170d4d5f39384477edd6669e16db79a4142
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 13:24:56 -0500

    Update WordNet

--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -1,8 +1,14 @@
-public class WordNet {
+/**
+ * The WordNet digraph, a rooted directed acyclic graph (DAG).
+ */
+public final class WordNet {
     /**
      * Constructor takes the name of the two input files.
      */
-    public WordNet(String synsets, String hypernyms) {}
+    public WordNet(String synsets, String hypernyms) {
+        validateArg(synsets);
+        validateArg(hypernyms);
+    }
  
     /**
      * Returns all WordNet nouns.
@@ -10,23 +16,49 @@ public class WordNet {
     public Iterable<String> nouns() {}
  
     /**
-     * Returns true if the word is a WordNet noun.
+     * Returns true if the WORD is a WordNet noun.
      */
-    public boolean isNoun(String word) {}
+    public boolean isNoun(String word) {
+        validateArg(word);
+    }
  
     /**
-     * Distance between nounA and nounB (defined below).
+     * Distance between nounA and nounB.
      */
-    public int distance(String nounA, String nounB) {}
+    public int distance(String nounA, String nounB) {
+        validateArg(nounA);
+        validateArg(nounB);
+        validateNoun(nounA);
+        validateNoun(nounB);
+    }
  
     /**
      * A synset (second field of synsets.txt) that is the common ancestor of
      * nounA and nounB.
      */
-    public String sap(String nounA, String nounB) {}
+    public String sap(String nounA, String nounB) {
+        validateArg(nounA);
+        validateArg(nounB);
+        validateNoun(nounA);
+        validateNoun(nounB);
+    }
  
     /**
      * Do unit testing of this class.
      */
     public static void main(String[] args) {}
+
+    private static void validateArg(Object arg) {
+        if (arg == null) {
+            String error = "Argument cannot be null";
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private void validateNoun(String noun) {
+        if (!isNoun(noun)) {
+            String error  = "Noun is not a WordNet noun";
+            throw new IllegalArgumentException(error);
+        }
+    }
 }
