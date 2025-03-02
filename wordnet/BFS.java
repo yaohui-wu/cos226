9155643cdec9e504f42bd64738afc8aea727c107
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-01 21:31:00 -0500

    Update Breadth-first search

--- wordnet/BFS.java
+++ wordnet/BFS.java
@@ -4,11 +4,11 @@ import java.util.Deque;
 import edu.princeton.cs.algs4.Digraph;
 
 /**
- * Breadth-first search.
+ * Breadth-first search (BFS).
  * 
  * @author Yaohui Wu
  */
-public class BreadthFirstSearch {
+public class BFS {
     private static final int INF = Integer.MAX_VALUE; // Represents infinity.
     // visited[v] = true if there is a path from s to v.
     private boolean[] visited;
@@ -17,7 +17,7 @@ public class BreadthFirstSearch {
     // distTo[v] = number of edges in the shortest path from s to v.
     private int[] distTo;
 
-    public BreadthFirstSearch(Digraph g, int s) {
+    public BFS(Digraph g, int s) {
         int order = g.V(); // Number of vertices in the graph.
         visited = new boolean[order];
         edgeTo = new int[order];
