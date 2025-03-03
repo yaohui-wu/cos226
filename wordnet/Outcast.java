2ff9325a8511260e4987e1a2841b7dd52288ba8c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-02 23:02:56 -0500

    Update outcast detection

--- wordnet/Outcast.java
+++ wordnet/Outcast.java
@@ -1,16 +1,39 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Outcast {
+public final class Outcast {
+    private final WordNet wordNet;
+
     /**
      * Constructor takes a WordNet object.
      */
-    public Outcast(WordNet wordnet) {}
+    public Outcast(WordNet wordnet) {
+        wordNet = wordnet;
+    }
 
     /**
-     * Given an array of WordNet nouns, return an outcast.
+     * Given an array of WordNet nouns, returns an outcast.
      */
-    public String outcast(String[] nouns) {}
+    public String outcast(String[] nouns) {
+        String outcast = nouns[0];
+        int max = Integer.MIN_VALUE;
+        int len = nouns.length;
+        for (int i = 0; i < len; i += 1) {
+            String nounX = nouns[i];
+            int dist = 0;
+            for (int j = 0; j < len; j += 1) {
+                String nounY = nouns[j];
+                if (i != j) {
+                    dist += wordNet.distance(nounX, nounY);
+                }
+            }
+            if (dist > max) {
+                max = dist;
+                outcast = nounX;
+            }
+        }
+        return outcast;
+    }
 
     /**
      * Test client.
