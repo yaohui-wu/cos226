7ec6d96e1bb0a907a5a682bf9e962f62eeaf3aae
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-03 23:00:42 -0500

    Update WordNet

--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
 
 /**
@@ -98,8 +99,7 @@ public final class WordNet {
     }
  
     /**
-     * A synset (second field of synsets.txt) that is the common ancestor of
-     * nounA and nounB.
+     * A synset that is the common ancestor of nounA and nounB.
      */
     public String sap(String nounA, String nounB) {
         validateArgs(nounA, nounB);
@@ -107,8 +107,8 @@ public final class WordNet {
         List<Integer> v = synsetsMap.get(nounA);
         List<Integer> w = synsetsMap.get(nounB);
         int ancestor = sap.ancestor(v, w);
-        String sap = synsetsList.get(ancestor);
-        return sap;
+        String synset = synsetsList.get(ancestor);
+        return synset;
     }
  
     /**
@@ -123,7 +123,7 @@ public final class WordNet {
         System.out.println("distance = " + distance + ", sap = " + sap);
     }
 
-    private static void validateArgs(Object... args) {
+    private void validateArgs(Object... args) {
         for (Object arg : args) {
             if (arg == null) {
                 String error = "Argument cannot be null";
@@ -132,21 +132,21 @@ public final class WordNet {
         }
     }
 
-    private void validateDAG(Digraph graph) {
-        if (!hasOneRoot(graph)) {
+    private void validateDAG(Digraph g) {
+        if (!hasOneRoot(g)) {
             String error = "Graph does not have only one root";
             throw new IllegalArgumentException(error);
         }
-        if (hasCycle(graph)) {
+        if (hasCycle(g)) {
             String error = "Graph is not a directed acyclic graph";
             throw new IllegalArgumentException(error);
         }
     }
 
-    private boolean hasOneRoot(Digraph graph) {
+    private boolean hasOneRoot(Digraph g) {
         int count = 0;
-        for (int v = 0; v < graph.V(); v += 1) {
-            if (isRoot(graph, v)) {
+        for (int v = 0; v < g.V(); v += 1) {
+            if (isRoot(g, v)) {
                 count += 1;
                 if (count > 1) {
                     return false;
@@ -157,42 +157,17 @@ public final class WordNet {
         return hasOneRoot;
     }
 
-    private boolean isRoot(Digraph graph, int v) {
-        int out = graph.outdegree(v);
-        int in = graph.indegree(v);
+    private boolean isRoot(Digraph g, int v) {
+        int out = g.outdegree(v);
+        int in = g.indegree(v);
         boolean isRoot = (out == 0 && in > 0);
         return isRoot;
     }
 
-    private boolean hasCycle(Digraph graph) {
-        int order = graph.V();
-        boolean[] visited = new boolean[order];
-        boolean[] onStack = new boolean[order];
-        for (int v = 0; v < order; v += 1) {
-            if (dfs(graph, v, visited, onStack)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Depth-first search (DFS).
-     */
-    private boolean dfs(Digraph graph, int s, boolean[] visited, boolean[] onStack) {
-        visited[s] = true;
-        onStack[s] = true;
-        for (int v : graph.adj(s)) {
-            if (!visited[v]) {
-                if (dfs(graph, v, visited, onStack)) {
-                    return true;
-                }
-            } else if (onStack[v]) {
-                return true;
-            }
-        }
-        onStack[s] = false;
-        return false;
+    private boolean hasCycle(Digraph g) {
+        DirectedCycle c = new DirectedCycle(g);
+        boolean hasCycle = c.hasCycle();
+        return hasCycle;
     }
 
     private void validateNouns(String... nouns) {
