d5354d1bca4856edcd1fb22ddeedb0bd297913b5
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-10 14:54:30 -0400

    Update Burrows–Wheeler transform

---
+++ burrows/CircularSuffixArray.java
@@ -0,0 +1,22 @@
+public class CircularSuffixArray {
+
+    /**
+     * Circular suffix array of s.
+     */
+    public CircularSuffixArray(String s) {}
+
+    /**
+     * Length of s.
+     */
+    public int length() {}
+
+    /**
+     * Returns index of ith sorted suffix.
+     */
+    public int index(int i) {}
+
+    /**
+     * Unit testing.
+     */
+    public static void main(String[] args) {}
+}
