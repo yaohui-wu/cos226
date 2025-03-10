cfb7bf56dc06dd3b9dd7c76ad5e041f649bc89a1
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-10 14:38:35 -0400

    Update Boggle

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BoggleSolver {
     /**
      * Initializes the data structure using the given array of strings as the
@@ -18,4 +21,22 @@ public class BoggleSolver {
      * through Z.)
      */
     public int scoreOf(String word) {}
+
+    /**
+     * Test client takes the filename of a dictionary and the filename of a
+     * Boggle board as command-line arguments and prints out all valid words
+     * for the given board using the given dictionary.
+     */
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
 }
