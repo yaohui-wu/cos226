87ba65e7fcd9427ef96592c0fedd71400758fd06
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-17 14:00:29 -0400

    Update Burrows–Wheeler transform

--- burrows/CircularSuffixArray.java
+++ burrows/CircularSuffixArray.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 import edu.princeton.cs.algs4.StdOut;
 
 public class CircularSuffixArray {
     private final int length;
-    private final char[] suffixes; // Original suffixes.
-    private final char[] sorted; // Sorted suffixes.
+    private final Integer[] indices;
 
     /**
      * Circular suffix array of s.
@@ -13,10 +13,30 @@ public class CircularSuffixArray {
     public CircularSuffixArray(String s) {
         validateArg(s);
         length = s.length();
-        suffixes = s.toCharArray();
-        sorted = new char[length];
-        System.arraycopy(suffixes, 0, sorted, 0, length);
-        Arrays.sort(sorted);
+        indices = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            indices[i] = i;
+        }
+        Comparator<Integer> comparator = (idx1, idx2) -> {
+            int element = idx1;
+            // Skip equal characters.
+            while (s.charAt(idx1) == s.charAt(idx2)) {
+                // Compare the next character in the circular suffix.
+                idx1 = (idx1 + 1) % length;
+                idx2 = (idx2 + 1) % length;
+                // All characters are equal.
+                if (idx1 == element) {
+                    return 0;
+                }
+            }
+            // Compare characters in ascending order.
+            if (s.charAt(idx1) < s.charAt(idx2)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        };
+        Arrays.sort(indices, comparator);
     }
 
     private void validateArg(Object obj) {
@@ -38,9 +58,7 @@ public class CircularSuffixArray {
      */
     public int index(int i) {
         validateIndex(i);
-        char c = sorted[i];
-        int index = search(suffixes, c);
-        return index;
+        return indices[i];
     }
 
     private void validateIndex(int i) {
@@ -48,16 +66,6 @@ public class CircularSuffixArray {
             String error = "Index out of bounds";
             throw new IllegalArgumentException(error);
         }
-    }
-
-    private int search(char[] characters, char c) {
-        int len = characters.length;
-        for (int i = 0; i < len; i++) {
-            if (characters[i] == c) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     /**
