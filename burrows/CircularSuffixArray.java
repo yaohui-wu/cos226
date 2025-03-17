d7824837fe0941e2b39cb90c7c4ac615c88eb6e6
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-17 12:01:18 -0400

    Update Burrows–Wheeler transform

--- burrows/CircularSuffixArray.java
+++ burrows/CircularSuffixArray.java
@@ -1,19 +1,49 @@
+import java.util.Arrays;
+
 public class CircularSuffixArray {
+    private final int length;
+    private final char[] suffixes; // Original suffixes.
+    private final char[] sorted; // Sorted suffixes.
 
     /**
      * Circular suffix array of s.
      */
-    public CircularSuffixArray(String s) {}
+    public CircularSuffixArray(String s) {
+        validateArg(s);
+        length = s.length();
+        suffixes = s.toCharArray();
+        sorted = new char[length];
+        System.arraycopy(suffixes, 0, sorted, 0, length);
+        Arrays.sort(sorted);
+    }
+
+    private void validateArg(Object obj) {
+        if (obj == null) {
+            String error = "Argument cannot be null";
+            throw new IllegalArgumentException(error);
+        }
+    }
 
     /**
      * Length of s.
      */
-    public int length() {}
+    public int length() {
+        return length;
+    }
 
     /**
      * Returns index of ith sorted suffix.
      */
-    public int index(int i) {}
+    public int index(int i) {
+        validateIndex(i);
+    }
+
+    private void validateIndex(int i) {
+        if (i < 0 || i >= length) {
+            String error = "Index out of bounds";
+            throw new IllegalArgumentException(error);
+        }
+    }
 
     /**
      * Unit testing.
